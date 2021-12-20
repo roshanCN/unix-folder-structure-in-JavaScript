7817a1b49d3fa566c623bd7f1623e6f6f73042da
Roshan CN <72539879+roshanCN@users.noreply.github.com>
2021-12-20 15:53:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+	function simplify(path)
+	{
+		// using vector in place of stack
+		let v = [];
+		let n = path.length;
+		let ans = "";
+		for (let i = 0; i < n; i++) {
+			let dir = "";
+			
+			// forming the current directory.
+			while (i < n && path[i] != '/') {
+				dir += path[i];
+				i++;
+			}
+
+			// if ".." , we pop.
+			if (dir == "..") {
+				if (v.length > 0)
+					v.pop();
+			}
+			else if (dir == "." || dir == "") {
+				// do nothing (added for better understanding.)
+			}
+			else {
+				// push the current directory into the vector.
+				v.push(dir);
+			}
+		}
+
+		// forming the ans
+		for(let i of v) {
+			ans += "/" + i;
+		}
+
+		// vector is empty
+		if (ans == "")
+			return "/";
+
+		return ans;
+	}
+	
+	// absolute path which we have to simplify.
+	let Str = "./home/././folder1/folder2/../folder3";
+	let res = simplify(Str);
+	console.log(res);
+  console.log('./home/././././folder1')
+  console.log('./home/folder1')
+
+// This code is contributed by mukesh07.
+
